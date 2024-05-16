3273aa83f81166b5c2560d6f3ada414cd32089ee
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-05-16 18:32:10 +0200

    refactor: Added inclusion guard for the image loading.

--- main.c
+++ main.c
@@ -35,8 +35,6 @@ void kite_array_start_pos() {
 
     kite_center_rotation(kite_array[i].kite, &start_pos, 0);
     start_pos.x += 2 * kite_width;
-
-    // kite_array[i].kite_input_handler_active = false;
   }
 }
 
@@ -148,9 +146,11 @@ int main(void) {
   Vector2 pos = {GetScreenWidth() / 2.f, GetScreenHeight() / 2.f};
   printf("The POS:" VECTOR2_FMT "\n", Vector2_FMT_ARGS(pos));
 
+#ifdef LOADIMAGE
   Image background_image =
       LoadImage("/home/marvin/Entwicklung/c/tkbc/src/assets/raw.png");
   Texture2D background_texture = LoadTextureFromImage(background_image);
+#endif /* ifdef LOADIMAGE */
 
   InitAudioDevice();
   if (IsAudioDeviceReady()) {
@@ -183,12 +183,15 @@ int main(void) {
       EndDrawing();
     } else {
       BeginDrawing();
+#ifdef LOADIMAGE
       float scale_width = (float)GetScreenWidth() / background_texture.width;
       float scale_height = (float)GetScreenHeight() / background_texture.height;
       float scale = fmaxf(scale_width, scale_height);
       DrawTextureEx(background_texture, (Vector2){0, 0}, 0, scale, WHITE);
+#endif /* ifdef LOADIMAGE */
 
       kite_draw_kite_array();
+
       DrawFPS(pos.x, 10);
       EndDrawing();
     }
