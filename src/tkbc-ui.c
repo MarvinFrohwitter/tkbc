316d99070551c7fa8f185aedab46dcb727627903
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-09-12 17:36:18 +0200

    refactor: Changed the UI timeline rendering code to more readable format.

--- src/tkbc-ui.c
+++ src/tkbc-ui.c
@@ -45,17 +45,26 @@ void tkbc_ui_timeline(Env *env) {
   env->timeline_segments_width =
       env->timeline_segment_width * env->timeline_segments;
 
-  env->timeline_front.width =
+  if ((mouse_pos.x >= env->timeline_base.x + env->timeline_base.width) ||
+      (env->timeline_segments >= env->block_frames->count)) {
+    // Just for save UI drawing if the mouse is outside the right bounding box
+    // of the timeline, the alignment should always be filled completely.
+    env->timeline_front.width = env->timeline_base.width;
 
-      // Just for save ui draw in if the mouse outside the right bounding box of
-      // the timeline the alignment should always be filled completely.
-      (mouse_pos.x >= env->timeline_base.x + env->timeline_base.width) ||
-              (env->timeline_segments >= env->max_block_index)
-          ? env->timeline_base.width
+  } else {
 
-      // If there are frames to display provide a segment in the timeline
-      : env->timeline_segments > 0 ? env->timeline_segments_width
-                                   : mouse_pos.x - env->timeline_base.x;
+    // If there are frames to display provide a segment in the timeline
+    if (env->timeline_segments > 0) {
+      env->timeline_front.width = env->timeline_segments_width;
+    } else {
+
+      /* TODO: Consider a width of just 0 in that case where there is no frame
+      to display. This is likely not going to happen, because the scripts have
+      the init frame.
+       */
+      env->timeline_front.width = mouse_pos.x - env->timeline_base.x;
+    }
+  }
 
   if (env->timeline_hoverover || env->timeline_interaction) {
     DrawRectangleRec(env->timeline_base, ColorBrightness(BLACK, 0.3));
