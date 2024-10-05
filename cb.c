e8a7c823d1f00638b3c147a952c5f85fadc4b082
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-10-05 23:16:28 +0200

    build: Updated the cb method to support static and dynamic executable.

--- cb.c
+++ cb.c
@@ -1,7 +1,33 @@
+#include <string.h>
 #define CB_IMPLEMENTATION
 #include "cb.h"
 
-int main(void) {
+#define RAYLIBPATH "external/raylib-5.0/src"
+
+char *shift_args(int *argc, char ***argv) {
+  char *old_argv = **argv;
+
+  if (*argc > 0) {
+    *argc = *argc - 1;
+    *argv = *argv + 1;
+  } else
+    assert(*argc > 0 && "Error: No more arguments left!");
+
+  return old_argv;
+}
+
+int main(int argc, char *argv[]) {
+
+  char *prog_name = shift_args(&argc, &argv);
+  char *ldd = shift_args(&argc, &argv);
+
+  int dynamic = 0;
+  if (strncmp(ldd, "static", 6) == 0) {
+    dynamic = 1;
+  } else if (strncmp(ldd, "dynamic", 7) == 0) {
+    dynamic = 0;
+  }
+
   Cmd cmd = {0};
   cb_cmd_push(&cmd, "rm", "-rf", "build");
   if (!cb_run_sync(&cmd))
@@ -11,12 +37,12 @@ int main(void) {
   if (!cb_run_sync(&cmd))
     return 1;
 
-  cb_cmd_push(&cmd, "make", "-C", "./external/raylib-5.0/src");
+  cb_cmd_push(&cmd, "make", "-C", RAYLIBPATH);
   if (!cb_run_sync(&cmd))
     return 1;
 
   cb_cmd_push(&cmd, "cc");
-  INCLUDE(&cmd, "-I", "external/raylib-5.0/src");
+  INCLUDE(&cmd, "-I", RAYLIBPATH);
   INCLUDE(&cmd, "-I", "src/");
   CFLAGS(&cmd, "-x", "c");
   CFLAGS(&cmd, "-O3", "-pedantic", "-Wall", "-Wextra", "-ggdb");
@@ -25,12 +51,20 @@ int main(void) {
     return 1;
 
   cb_cmd_push(&cmd, "cc");
-  INCLUDE(&cmd, "-I", "external/raylib-5.0/src");
+  INCLUDE(&cmd, "-I", RAYLIBPATH);
   INCLUDE(&cmd, "-I", "tkbc_scripts/", "-I", "src/");
   CFLAGS(&cmd, "-x", "c");
   CFLAGS(&cmd, "-O3", "-pedantic", "-Wall", "-Wextra", "-ggdb");
-  LIBS(&cmd, "-lraylib", "-lm");
-  cb_cmd_push(&cmd, "-o", "build/tkbc", "src/main.c", "src/tkbc.c", "/src/tkbc-ui.c");
+  cb_cmd_push(&cmd, "-o", "build/tkbc", "src/main.c", "src/tkbc.c",
+              "src/tkbc-ui.c");
+    LDFLAGS(&cmd, "-L", RAYLIBPATH);
+
+  if (dynamic) {
+    LIBS(&cmd, "-l:libraylib.a", "-lm");
+  } else {
+    LIBS(&cmd, "-lraylib", "-lm");
+  }
+
   if (!cb_run_sync(&cmd))
     return 1;
 
