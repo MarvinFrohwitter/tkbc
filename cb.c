7eb8253c47f2f8246f5be872c0eff6bee79a7832
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-11-03 15:40:04 +0100

    refactor(cb): Made individual functions for every single compile step.

--- cb.c
+++ cb.c
@@ -2,6 +2,7 @@
 #define CB_IMPLEMENTATION
 #include "cb.h"
 
+#define CC "gcc"
 #define RAYLIBPATH "external/raylib-5.0/src/"
 #define CHOREOGRAPHERPATH "src/choreographer/"
 
@@ -17,139 +18,134 @@ char *shift_args(int *argc, char ***argv) {
   return old_argv;
 }
 
-int main(int argc, char *argv[]) {
+void include(Cmd *cmd) {
+  INCLUDE(cmd, "-I", RAYLIBPATH);
+  INCLUDE(cmd, "-I", "src/global/");
+  INCLUDE(cmd, "-I", "src/choreographer/");
+  INCLUDE(cmd, "-I", "src/network/");
+  INCLUDE(cmd, "-I", "tkbc_scripts/");
+  INCLUDE(cmd, "-I", "build/");
+}
 
+void cflags(Cmd *cmd) {
+  CFLAGS(cmd, "-x", "c");
+  CFLAGS(cmd, "-O3", "-pedantic", "-Wall", "-Wextra", "-ggdb");
+}
+
+void linker(Cmd *cmd, bool link_dynamic) {
+
+  LDFLAGS(cmd, "-L", RAYLIBPATH);
+  INCLUDE(cmd, "-L", "src/global/");
+  INCLUDE(cmd, "-L", "src/choreographer/");
+  INCLUDE(cmd, "-L", "src/network/");
+  INCLUDE(cmd, "-L", "tkbc_scripts/");
+
+  if (link_dynamic) {
+    LDFLAGS(cmd, "-Wl,-rpath=" RAYLIBPATH);
+    LDFLAGS(cmd, "-Wl,-rpath=../" RAYLIBPATH);
+    LIBS(cmd, "-l:libraylib.so", "-lm");
+  } else {
+    LIBS(cmd, "-l:libraylib.a", "-lm");
+  }
+}
+
+void pre_build(Cmd *cmd) {
+  cb_cmd_push(cmd, "rm", "-rf", "build");
+  if (!cb_run_sync(cmd))
+    exit(EXIT_FAILURE);
+
+  cb_cmd_push(cmd, "mkdir", "-p", "build");
+  if (!cb_run_sync(cmd))
+    exit(EXIT_FAILURE);
+}
+
+void raylib(Cmd *cmd, bool link_dynamic) {
+  // Build raylib
+  if (link_dynamic) {
+    cb_cmd_push(cmd, "make", "-e", "RAYLIB_LIBTYPE=SHARED", "-C", RAYLIBPATH);
+  } else {
+    cb_cmd_push(cmd, "make", "-C", RAYLIBPATH);
+  }
+  if (!cb_run_sync(cmd))
+    exit(EXIT_FAILURE);
+}
+
+void kite_script(Cmd *cmd) {
+  // Kite Script
+  cb_cmd_push(cmd, CC);
+  include(cmd);
+  cflags(cmd);
+  cb_cmd_push(cmd, "-o", "build/first.o", "-c", "tkbc_scripts/first.c");
+  if (!cb_run_sync(cmd))
+    exit(EXIT_FAILURE);
+}
+
+void choreographer(Cmd *cmd, bool link_dynamic) {
+  // Choreographer
+  cb_cmd_push(cmd, CC);
+  include(cmd);
+  cflags(cmd);
+  cb_cmd_push(cmd, "-o", "build/tkbc");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "main.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc-ffmpeg.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc-input-handler.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc-script-api.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc-script-handler.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc-sound-handler.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc-team-figures-api.c");
+  cb_cmd_push(cmd, CHOREOGRAPHERPATH "tkbc-ui.c");
+  linker(cmd, link_dynamic);
+  if (!cb_run_async(cmd))
+    exit(EXIT_FAILURE);
+}
+
+void server(Cmd *cmd, bool link_dynamic) {
+  // Server
+  cb_cmd_push(cmd, CC);
+  include(cmd);
+  cflags(cmd);
+  cb_cmd_push(cmd, "-o", "build/server");
+  cb_cmd_push(cmd, "src/network/tkbc-server.c");
+  cb_cmd_push(cmd, "src/network/tkbc-server-client-handler.c");
+  cb_cmd_push(cmd, "src/network/tkbc-network-common.c");
+  linker(cmd, link_dynamic);
+  if (!cb_run_async(cmd))
+    exit(EXIT_FAILURE);
+}
+
+
+void client(Cmd *cmd, bool link_dynamic) {
+  // Client
+  cb_cmd_push(cmd, CC);
+  include(cmd);
+  cflags(cmd);
+  cb_cmd_push(cmd, "-o", "build/client");
+  cb_cmd_push(cmd, "src/network/tkbc-client.c");
+  cb_cmd_push(cmd, "src/network/tkbc-network-common.c");
+  linker(cmd, link_dynamic);
+  if (!cb_run_sync(cmd))
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
   char *prog_name = shift_args(&argc, &argv);
   char *ldd = shift_args(&argc, &argv);
 
-  int dynamic = 0;
+  int linkoption = 1;
   if (strncmp(ldd, "static", 6) == 0) {
-    dynamic = 0;
+    linkoption = 0;
   } else if (strncmp(ldd, "dynamic", 7) == 0) {
-    dynamic = 1;
+    linkoption = 1;
   }
 
   Cmd cmd = {0};
-  cb_cmd_push(&cmd, "rm", "-rf", "build");
-  if (!cb_run_sync(&cmd))
-    return 1;
-
-  cb_cmd_push(&cmd, "mkdir", "-p", "build");
-  if (!cb_run_sync(&cmd))
-    return 1;
-
-  if (dynamic) {
-    cb_cmd_push(&cmd, "make", "-e", "RAYLIB_LIBTYPE=SHARED", "-C", RAYLIBPATH);
-  } else {
-    cb_cmd_push(&cmd, "make", "-C", RAYLIBPATH);
-  }
-
-  if (!cb_run_sync(&cmd))
-    return 1;
-
-  cb_cmd_push(&cmd, "cc");
-  INCLUDE(&cmd, "-I", RAYLIBPATH);
-
-  INCLUDE(&cmd, "-I", "src/choreographer/");
-  INCLUDE(&cmd, "-I", "src/global/");
-  INCLUDE(&cmd, "-I", "src/network/");
-
-  CFLAGS(&cmd, "-x", "c");
-  CFLAGS(&cmd, "-O3", "-pedantic", "-Wall", "-Wextra", "-ggdb");
-  cb_cmd_push(&cmd, "-o", "build/first.o", "-c", "tkbc_scripts/first.c");
-  if (!cb_run_sync(&cmd))
-    return 1;
-
-  cb_cmd_push(&cmd, "cc");
-  INCLUDE(&cmd, "-I", RAYLIBPATH);
-
-  INCLUDE(&cmd, "-I", "tkbc_scripts/");
-  INCLUDE(&cmd, "-I", "src/choreographer/");
-  INCLUDE(&cmd, "-I", "src/global/");
-  INCLUDE(&cmd, "-I", "src/network/");
-
-  CFLAGS(&cmd, "-x", "c");
-  CFLAGS(&cmd, "-O3", "-pedantic", "-Wall", "-Wextra", "-ggdb");
-  cb_cmd_push(&cmd, "-o", "build/tkbc");
-
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "main.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc-ffmpeg.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc-input-handler.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc-script-api.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc-script-handler.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc-sound-handler.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc-team-figures-api.c");
-  cb_cmd_push(&cmd, CHOREOGRAPHERPATH "tkbc-ui.c");
-
-  LDFLAGS(&cmd, "-L", RAYLIBPATH);
-  if (dynamic) {
-    LDFLAGS(&cmd, "-Wl,-rpath=" RAYLIBPATH);
-    LDFLAGS(&cmd, "-Wl,-rpath=../" RAYLIBPATH);
-    LIBS(&cmd, "-l:libraylib.so", "-lm");
-  } else {
-    LIBS(&cmd, "-l:libraylib.a", "-lm");
-  }
-
-  if (!cb_run_sync(&cmd))
-    return 1;
-
-  // Server
-  cb_cmd_push(&cmd, "cc");
-  INCLUDE(&cmd, "-I", RAYLIBPATH);
-  INCLUDE(&cmd, "-I", "src/choreographer/");
-  INCLUDE(&cmd, "-I", "src/global/");
-  INCLUDE(&cmd, "-I", "src/network/");
-  INCLUDE(&cmd, "-I", "tkbc_scripts/");
-  INCLUDE(&cmd, "-I", "build/");
-  CFLAGS(&cmd, "-x", "c");
-  CFLAGS(&cmd, "-O3", "-pedantic", "-Wall", "-Wextra", "-ggdb");
-
-  cb_cmd_push(&cmd, "-o", "build/server");
-
-  cb_cmd_push(&cmd, "src/network/tkbc-server.c");
-  cb_cmd_push(&cmd, "src/network/tkbc-server-client-handler.c");
-  cb_cmd_push(&cmd, "src/network/tkbc-network-common.c");
-
-  LDFLAGS(&cmd, "-L", RAYLIBPATH);
-  if (dynamic) {
-    LDFLAGS(&cmd, "-Wl,-rpath=" RAYLIBPATH);
-    LDFLAGS(&cmd, "-Wl,-rpath=../" RAYLIBPATH);
-    LIBS(&cmd, "-l:libraylib.so", "-lm");
-  } else {
-    LIBS(&cmd, "-l:libraylib.a", "-lm");
-  }
-
-  if (!cb_run_sync(&cmd))
-    return 1;
-
-  // Client
-  cb_cmd_push(&cmd, "cc");
-  INCLUDE(&cmd, "-I", RAYLIBPATH);
-  INCLUDE(&cmd, "-I", "src/choreographer/");
-  INCLUDE(&cmd, "-I", "src/global/");
-  INCLUDE(&cmd, "-I", "src/network/");
-  INCLUDE(&cmd, "-I", "tkbc_scripts/");
-  INCLUDE(&cmd, "-I", "build/");
-  CFLAGS(&cmd, "-x", "c");
-  CFLAGS(&cmd, "-O3", "-pedantic", "-Wall", "-Wextra", "-ggdb");
-
-  cb_cmd_push(&cmd, "-o", "build/client");
-
-  cb_cmd_push(&cmd, "src/network/tkbc-client.c");
-  cb_cmd_push(&cmd, "src/network/tkbc-network-common.c");
-
-  LDFLAGS(&cmd, "-L", RAYLIBPATH);
-  if (dynamic) {
-    LDFLAGS(&cmd, "-Wl,-rpath=" RAYLIBPATH);
-    LDFLAGS(&cmd, "-Wl,-rpath=../" RAYLIBPATH);
-    LIBS(&cmd, "-l:libraylib.so", "-lm");
-  } else {
-    LIBS(&cmd, "-l:libraylib.a", "-lm");
-  }
-
-  if (!cb_run_sync(&cmd))
-    return 1;
+  pre_build(&cmd);
+  raylib(&cmd, linkoption);
+  kite_script(&cmd);
+  choreographer(&cmd, linkoption);
+  server(&cmd, linkoption);
+  client(&cmd, linkoption);
 
   return 0;
 }
