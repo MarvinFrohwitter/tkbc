d4c9221b03ad68b54a3be2fd329f373a6be6cac3
Marvin Frohwitter <marvin.frohwitter@web.de>
2025-11-08 17:01:33 +0100

    fix: Deduplicated MESSAGE_SCRIPT_META_DATA from the interface.h.

--- src/network/tkbc-interface.h
+++ src/network/tkbc-interface.h
@@ -2,13 +2,22 @@
 typedef enum {
   MESSAGE_ZERO = 0,
   MESSAGE_HELLO,
-  MESSAGE_KITEADD,
+
+  MESSAGE_KITEADD, // When a kite is added because a new client has connected to
+                   // the server
+  MESSAGE_KITEVALUE, // Update from the client to the server -> the server
+                     // broadcasts the information except to the client where
+                     // it's coming from.
+
   MESSAGE_CLIENT_DISCONNECT,
-  MESSAGE_CLIENTKITES,
+
+  MESSAGE_CLIENTKITES, // From server to client in the beginning to inform the
+                       // client about all kites and when a acript is running.
   MESSAGE_KITES,
+
   MESSAGE_KITES_POSITIONS,
   MESSAGE_KITES_POSITIONS_RESET,
-  MESSAGE_KITEVALUE,
+
   MESSAGE_SCRIPT,
   MESSAGE_SCRIPT_AMOUNT,
   MESSAGE_SCRIPT_PARSED,
@@ -54,7 +63,8 @@ typedef enum {
 /////////////////// Duplicate ?
 /**
  *
- * MESSAGE_CLIENTKITES: From server to client
+ * MESSAGE_CLIENTKITES: From server to client in the beginning to inform the
+ * client about all kites and when a acript is running.
  *
  *****
  * MESSAGE_CLIENTKITES:active_count:[kite_id:(x,y):angle:color:texture_id:is_reversed:]^*\r\n
@@ -128,15 +138,6 @@ typedef enum {
  *
  *****
  * MESSAGE_SCRIPT_PARSED:\r\n
- *****
- */
-
-/**
- *
- * MESSAGE_SCRIPT_META_DATA:
- *
- *****
- * MESSAGE_SCRIPT_META_DATA:script_id:script_count:frames_index:\r\n
  *****
  */
 
