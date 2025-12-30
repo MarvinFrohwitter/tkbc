1ea6ffcde6c5280f75315aab41097fb04d0cd8ed
Marvin Frohwitter <marvin.frohwitter@web.de>
2025-12-30 03:01:13 +0100

    docs: Added an explanation of the BUG about a script, where KITE_MOVE_ADD() and KITE_TIP_ROTATION_ADD() are used in the same SET().

--- tkbc_scripts/first.c
+++ tkbc_scripts/first.c
@@ -2,10 +2,10 @@
 #include "../src/choreographer/tkbc-team-figures-api.h"
 
 // Some test scripts
+#include "demo_angle_left_right_rotation.c"
 #include "demo_rotation_checkup.c"
 #include "demo_split_roll.c"
 #include "demo_team_function_demonstration.c"
-#include "demo_angle_left_right_rotation.c"
 
 #include "demo_choreo.c"
 
@@ -23,10 +23,21 @@ tkbc_script_input {
 
   //////////////////////////////////////////////////////////////////////
   //
-  // BUG
-  // tkbc_script_begin();
-  // SET(
-
+  // BUG At the moment KITE_MOVE_ADD() and KITE_TIP_ROTATION_ADD() can't be
+  //     called in the same SET(), because they internally uses not the same
+  //     reposition function and the TIP location computation messes with moving
+  //     the kite first.
+  //     So basically the KITE_MOVE_ADD can overshoot and don't terminate,
+  //     because the TIP computation moves the kite as well.
+  //
+  //     The solution would be to check for move and tip rotations explicitly.
+  //     An compute the combined endposition up front and patch the potions
+  //     where the kite should move to.
+  //
+  // Tho the workaround for now is to just call the normal rotation an move to
+  // the correct position manually.
+  //
+  // tkbc_script_begin(); SET(
   //     KITE_MOVE_ADD(ID(0), -kite.width, 0, rotation_duration),
   //     KITE_MOVE_ADD(ID(1), kite.width, 0, rotation_duration),
 
