6a193e1b24cf9f51469f377dd4cf58f81deedb48
Marvin Frohwitter <marvin.frohwitter@web.de>
2026-09-21 17:49:24 +0200

    fix: Made the thing compile again.

--- src/choreographer/tkbc.c
+++ src/choreographer/tkbc.c
@@ -787,3 +787,76 @@ bool tkbc_set_kite_texture(Kite *kite, Kite_Texture *kite_texture) {
     kite->texture.normal = kite_texture->normal;
     return IsTextureValid(kite->texture.normal);
 }
+
+/**
+ * @brief Checks for all keys that can be a valid hex key. If there are pressed
+ * one or pressed repeat.
+ *
+ * @return The hex color key that is down or 0 if no color key is down.
+ */
+KeyboardKey tkbc_is_hex_color_key_down() {
+    static KeyboardKey keys[] = {
+        KEY_ZERO,  KEY_ONE,  KEY_TWO, KEY_THREE, KEY_FOUR, KEY_FIVE, KEY_SIX, KEY_SEVEN,
+        KEY_EIGHT, KEY_NINE, KEY_A,   KEY_B,     KEY_C,    KEY_D,    KEY_E,   KEY_F,
+    };
+
+    for (KeyboardKey key = 0; key < ARRAY_LENGTH(keys); ++key) {
+        if (IsKeyPressedRepeat(keys[key]) || IsKeyPressed(keys[key])) {
+            return keys[key];
+        }
+    }
+    return KEY_NULL;
+}
+
+/**
+ * @brief [TODO:description]
+ *
+ * @return [TODO:return]
+ */
+KeyboardKey tkbc_is_port_number_key_down() {
+    static KeyboardKey keys[] = {
+        KEY_ZERO,   KEY_ONE, KEY_TWO, KEY_THREE, KEY_FOUR, KEY_FIVE, KEY_SIX, KEY_SEVEN, KEY_EIGHT, KEY_NINE,
+
+        KEY_PERIOD,
+    };
+
+    for (KeyboardKey key = 0; key < ARRAY_LENGTH(keys); ++key) {
+        if (IsKeyPressedRepeat(keys[key]) || IsKeyPressed(keys[key])) {
+            return keys[key];
+        }
+    }
+    return KEY_NULL;
+}
+
+/**
+ * @brief [TODO:description]
+ *
+ * @return [TODO:return]
+ */
+KeyboardKey tkbc_is_domain_name_key_down() {
+    static KeyboardKey keys[] = {
+        KEY_ZERO,   KEY_ONE,   KEY_TWO, KEY_THREE, KEY_FOUR, KEY_FIVE, KEY_SIX, KEY_SEVEN, KEY_EIGHT, KEY_NINE,
+
+        KEY_A,      KEY_B,     KEY_C,   KEY_D,     KEY_E,    KEY_F,    KEY_G,   KEY_H,     KEY_I,     KEY_J,
+        KEY_K,      KEY_L,     KEY_M,   KEY_N,     KEY_O,    KEY_P,    KEY_Q,   KEY_R,     KEY_S,     KEY_T,
+        KEY_U,      KEY_V,     KEY_W,   KEY_X,     KEY_Y,    KEY_Z,
+
+        KEY_PERIOD, KEY_MINUS,
+    };
+
+    for (KeyboardKey key = 0; key < ARRAY_LENGTH(keys); ++key) {
+        if (IsKeyPressedRepeat(keys[key]) || IsKeyPressed(keys[key])) {
+            return keys[key];
+        }
+    }
+    return KEY_NULL;
+}
+
+KeyboardKey tkbc_is_any_ascii_key_down() {
+    for (int key = ' '; key < 127; ++key) {
+        if (IsKeyPressedRepeat(key) || IsKeyPressed(key)) {
+            return key;
+        }
+    }
+    return KEY_NULL;
+}
